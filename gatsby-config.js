00f650ad0377d0475f35c628da225d97cea4f67c
Nicolas Kleiderer <nakleiderer@gmail.com>
2019-03-10 19:09:48 -0500

    Add plugin to improve netlify cache

--- gatsby-config.js
+++ gatsby-config.js
@@ -30,6 +30,7 @@ module.exports = {
     'gatsby-transformer-sharp',
     'gatsby-plugin-react-helmet',
     'gatsby-plugin-sharp',
+    'gatsby-plugin-netlify-cache',
     {
       resolve: 'gatsby-source-contentful',
       options: contentfulConfig,
