5b6432e6eeeb666af75ccb24141662d464fe0c27
Nicolas Kleiderer <nakleiderer@gmail.com>
2019-03-28 18:01:18 -0500

    Make hero text display over image

--- src/components/hero.js
+++ src/components/hero.js
@@ -1,23 +1,56 @@
-import { Button, Grid, Typography, withStyles } from '@material-ui/core'
+import {
+  Button,
+  Grid,
+  Typography,
+  withStyles,
+  createMuiTheme,
+} from '@material-ui/core'
 import Img from 'gatsby-image'
 import PropTypes from 'prop-types'
 import React from 'react'
 
+const darkTheme = createMuiTheme({ palette: { type: 'dark' } })
+
 const styles = theme => ({
-  heroUnit: {
-    // backgroundColor: theme.palette.background.paper,
+  heroUnitImage: {
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+    justifyContent: 'center',
+    background: 'rgba(0, 0, 0, 0.60)',
+    width: '100%',
+    height: '100%',
+    top: '50%',
+    left: '50%',
+    transform: 'translate(-50%, -50%)',
+    position: 'absolute',
+  },
+  heroUnitNoImage: {
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+    justifyContent: 'center',
+    background: 'rgba(0, 0, 0, 0.60)',
+    width: '100%',
+    height: '100%',
   },
   heroContent: {
-    maxWidth: 600,
-    margin: '0 auto',
     padding: `${theme.spacing.unit * 6}px ${theme.spacing.unit}px ${theme
       .spacing.unit * 6}px ${theme.spacing.unit}px`,
   },
   heroButtons: {
     marginTop: theme.spacing.unit * 4,
   },
-  image: {
+  container: {
+    position: 'relative',
+    width: '100%',
     height: 593,
+  },
+  primaryText: {
+    color: darkTheme.palette.text.primary,
+  },
+  secondaryText: {
+    color: darkTheme.palette.text.secondary,
   },
 })
 
@@ -25,25 +58,21 @@ const imgStyle = { objectFit: 'cover', height: 593 }
 
 function Hero({ classes, title, subtitle, description, heroImage }) {
   const shouldRender = title || subtitle || description || heroImage
+  const heroUnitClassName = heroImage
+    ? classes.heroUnitImage
+    : classes.heroUnitNoImage
   return (
     <div>
       {shouldRender && (
-        <div>
-          {heroImage && (
-            <Img
-              className={classes.image}
-              alt={title}
-              imgStyle={imgStyle}
-              {...heroImage}
-            />
-          )}
-          <div className={classes.heroUnit}>
+        <div className={classes.container}>
+          {heroImage && <Img alt={title} imgStyle={imgStyle} {...heroImage} />}
+          <div className={heroUnitClassName}>
             <div className={classes.heroContent}>
               <Typography
                 component="h1"
                 variant="h3"
                 align="center"
-                color="textPrimary"
+                className={classes.primaryText}
                 gutterBottom={!subtitle}
               >
                 {title}
@@ -53,7 +82,7 @@ function Hero({ classes, title, subtitle, description, heroImage }) {
                   component="p"
                   variant="h5"
                   align="center"
-                  color="textPrimary"
+                  className={classes.primaryText}
                   gutterBottom
                 >
                   {subtitle}
@@ -63,26 +92,26 @@ function Hero({ classes, title, subtitle, description, heroImage }) {
                 <Typography
                   variant="h6"
                   align="center"
-                  color="textSecondary"
+                  className={classes.secondaryText}
                   paragraph
                 >
                   {description}
                 </Typography>
               )}
               {/* <div className={classes.heroButtons}>
-            <Grid container spacing={16} justify="center">
-              <Grid item>
-                <Button variant="contained" color="primary">
-                  Main call to action
-                </Button>
-              </Grid>
-              <Grid item>
-                <Button variant="outlined" color="primary">
-                  Secondary action
-                </Button>
-              </Grid>
-            </Grid>
-          </div> */}
+                <Grid container spacing={16} justify="center">
+                  <Grid item>
+                    <Button variant="contained" color="primary">
+                      Main call to action
+                    </Button>
+                  </Grid>
+                  <Grid item>
+                    <Button variant="outlined" color="primary">
+                      Secondary action
+                    </Button>
+                  </Grid>
+                </Grid>
+              </div> */}
             </div>
           </div>
         </div>
