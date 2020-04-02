7353c1c0482414773795bc90f322bd845f227976
Nicolas Kleiderer <nicolas.kleiderer@daveramsey.com>
2020-04-02 01:11:47 +0000

    format primary template

--- site/src/_includes/layouts/primary.11ty.js
+++ site/src/_includes/layouts/primary.11ty.js
@@ -18,7 +18,11 @@ class PrimaryLayout {
             content="width=device-width, initial-scale=1.0"
           />
           <title>${pageTitle}</title>
-          <link rel="preconnect" href="https://fonts.gstatic.com/" crossorigin>
+          <link
+            rel="preconnect"
+            href="https://fonts.gstatic.com/"
+            crossorigin
+          />
           <link
             href="https://fonts.googleapis.com/css2?family=Rokkitt:wght@400;600;700&display=swap"
             rel="stylesheet"
