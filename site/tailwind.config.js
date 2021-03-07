b2395d8a3a59a9f039152408861368e04e5059d9
Nicolas Kleiderer <4278631+nakleiderer@users.noreply.github.com>
2021-03-07 14:25:16 -0600

    improve article styling (#727)

--- site/tailwind.config.js
+++ site/tailwind.config.js
@@ -1,12 +1,17 @@
 const colors = require("tailwindcss/colors");
 
+const textColorEls = ["blockquote", "h1", "h2", "h3", "h4", "h5", "h6"];
+const textColors = (theme) => textColorEls.reduce((p, el) => {
+    p[el] = { color: theme("colors.black") };
+    return p;
+  }, {});
+
 module.exports = {
   purge: ["./src/**/*.ejs", "./src/**/*.md"],
   theme: {
     fontFamily: {
       sans: ["Work Sans", "sans-serif"],
     },
-    extend: {},
     colors: {
       transparent: "transparent",
       current: "currentColor",
@@ -15,6 +20,16 @@ module.exports = {
       teal: colors.teal,
       white: colors.teal[50],
       black: colors.teal[900],
+    },
+    extend: {
+      typography: (theme) => ({
+        DEFAULT: {
+          css: {
+            color: theme("colors.black"),
+            ...textColors(theme),
+          },
+        },
+      }),
     },
   },
   variants: {},
